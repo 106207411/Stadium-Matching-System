8b601fa4a0a04d1be63d83e1699cb68897ec6c4b
yihsuan <yiiihsuann@gmail.com>
2023-11-23 23:37:53 +0800

    feat: Home & navigate to activity and sports, switch from List/Map, logOut

--- stadium-app/src/pages/Home/Home.jsx
+++ stadium-app/src/pages/Home/Home.jsx
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useAuth } from "../../context/AuthContext";
 import './Home.scss';
 import FooterBar from "../../components/FooterBar/FooterBar";
+import { PiSignOutBold } from "react-icons/pi";
+import { useNavigate } from 'react-router-dom';
+import { GoSearch } from "react-icons/go";
 
 const Home = () => {
   const { logout } = useAuth();
@@ -9,84 +12,104 @@ const Home = () => {
   const sports = ['tennis', 'tabletennis', 'badminton', 'basketball', 'volley', 'baseball', 'gym', 'swimming'];
   const activities = ['b1', 'b2'];
 
+  const navigate = useNavigate();
+
+  const gotoStadium = () => {
+    navigate('/stadium/list');
+  };
+
+  const gotoActivity = () => {
+    navigate('/activity/list');
+  };
+
+  const [selectedOption, setSelectedOption] = useState('List'); // 預設選List
+
+
+  const toggleOption = (option) => {
+    setSelectedOption(option);
+  };
+
+  const handleSearchIconClick = () => {
+    const exampleId = '123';
+    navigate(`/activity/${exampleId}`);
+  };
+
   return (
     <div className="home-container">
-        <div class="button-container">
-          <button onClick={logout}>Logout</button>
-        </div>
+      <div className="button-container" onClick={logout}>
+        <PiSignOutBold />
+      </div>
 
       <div className="search-bar">
-        <input type="search" placeholder="Search for a sport or venue" />
+        <input type="search" placeholder="Search for activities..." />
+        <GoSearch className="search-icon" onClick={handleSearchIconClick} />
       </div>
 
       <div className="options-container">
-      <div className="options-section">
-        <button className="option active">Map</button>
-        <button className="option">List</button>
-      </div>
-      </div>
+        <div className="options-section">
 
-
-      <div className="sports-container">
-        <div className="title-container">
-          <div className="title-left">場地</div>
-          <div className="title-right">
-            <a href="#">更多</a>
-          </div>
+          <button
+            className={`option ${selectedOption === 'List' ? 'active' : ''}`}
+            onClick={() => toggleOption('List')}>
+            List
+          </button>
+          <button
+            className={`option ${selectedOption === 'Map' ? 'active' : ''}`}
+            onClick={() => toggleOption('Map')}>
+            Map
+          </button>
         </div>
-        <div className="sports-section">
-          {sports.map(sport => (
-            <div key={sport} className="sport">
-              <img src={`/${sport}.png`} alt={sport} />
+      </div>
+      {selectedOption === 'List' ? (
+        <>
+
+
+          <div className="sports-container">
+            <div className="title-container">
+              <div className="title-left">場地</div>
+              <div className="title-right">
+                <span onClick={gotoStadium} style={{ cursor: 'pointer' }}>更多</span>
+              </div>
             </div>
-          ))}
-        </div>
-      </div>
-
-
-
-      <div className="activity-container">
-        <div className="title-container">
-          <div className="title-left">活動</div>
-          <div className="title-right">
-            <a href="#">更多</a>
+            <div className="sports-section">
+              {sports.map(sport => (
+                <div key={sport} className="sport">
+                  <img src={`/${sport}.png`} alt={sport} />
+                </div>
+              ))}
+            </div>
           </div>
-        </div>
-        <div className="activity-section">
-        {activities.map(activity => (
-          <div key={activity} className="activity">
-            <img src={`/${activity}.jpg`} alt={activity} />
-          </div>
-        ))}
-        </div>
-      </div>
 
+          <div className="activity-container">
+            <div className="title-container">
+              <div className="title-left">活動</div>
+
+              <div className="title-right">
+                <span onClick={gotoActivity} style={{ cursor: 'pointer' }}>更多</span>
+              </div>
+            </div>
+            <div className="activity-section">
+              {activities.map(activity => (
+                <div key={activity} className="activity">
+                  <img src={`/${activity}.jpg`} alt={activity} />
+                </div>
+              ))}
+            </div>
+          </div>
+
+        </>
+      ) : (
+        <>
+          <div className="map-container">
+            <div style={{ textAlign: 'center' }}>
+              to be continued...
+            </div>
+          </div>
+        </>
+      )}
       <FooterBar />
     </div>
   );
 };
 
 export default Home;
-
-
-/*
-import { useAuth } from "../../context/AuthContext"
-import FooterBar from "../../components/FooterBar/FooterBar"
-import './Home.scss'
-
-const Home = () => {
-  const { logout } = useAuth()
-
-  return (
-    <div className="container">
-      <div className="content">
-        <h1>Home Page</h1>
-        <button onClick={logout}>Logout</button>
-      </div>
-      <FooterBar />
-    </div>
-  )
-}
-
-export default Home
-*/
