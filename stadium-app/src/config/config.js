1874b342d347fbf4a2885530af2d0565fdd7d995
Cody Chen <109569898+cdxvy30@users.noreply.github.com>
2023-12-06 23:27:25 +0800

    Update config.js
    HTTPs to HTTP

--- stadium-app/src/config/config.js
+++ stadium-app/src/config/config.js
@@ -1,5 +1,5 @@
 export const API_URL= 'http://localhost:3000/api'
-export const PROD_API_URL = 'https://52.8.178.204/api'
+export const PROD_API_URL = 'http://52.8.178.204/api'
 
 export const baseballThumbnail = ''
 export const tableTennisThumbnail = ''
