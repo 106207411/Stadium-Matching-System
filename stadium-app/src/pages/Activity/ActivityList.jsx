08128925812989e5b7b2cf3ed2399eaf6c500392
yihsuan <yiiihsuann@gmail.com>
2023-12-18 16:28:58 +0800

    feat: join and leave activity api, activity detail page

--- stadium-app/src/pages/Activity/ActivityList.jsx
+++ stadium-app/src/pages/Activity/ActivityList.jsx
@@ -3,9 +3,12 @@ import './ActivityList.scss';
 import FooterBar from '../../components/FooterBar/FooterBar.jsx'; 
 import Header from '../../components/Header/Header.jsx'; 
 import mockActivities from '../../mockData/mockActivities.js'; 
+import { useNavigate } from 'react-router-dom';
 import { useQuery } from '@tanstack/react-query';
 import { fetchActivities } from '../../api';
+import LoadingSpinner from '../../components/Loading/LoadingPage'; 
 import InfiniteScroll from 'react-infinite-scroll-component'; // Make sure to install this package
+import { FaStar } from 'react-icons/fa';
 
 const ActivityList = () => {
   // const [activities, setActivities] = useState([]);
@@ -20,44 +23,17 @@ const ActivityList = () => {
   console.log('Activities data:', activitiesData);
 
 
-  // useEffect(() => {
-  //   // Load mock data instead of fetching from an API
-  //   setActivities(mockActivities);
-  // }, []);
+  const generateStars = (rating) => {
+    let stars = [];
+    for (let i = 0; i < rating; i++) {
+      stars.push(<FaStar key={i} className="star" />);
+    }
+    return stars;
+  };
 
-  // useEffect(() => {
-  //   // Initial load
-  //   if (activities.length > 0) {
-  //     setVisibleActivities(activities.slice(0, 10)); // 初始顯示10個活動
-  //   }
-  // }, [activities]);
-
-  // const loadMoreActivities = () => {
-  //   // 載入更多活動
-  //   const currentLength = visibleActivities.length;
-  //   const newVisibleActivities = activities.slice(
-  //     currentLength,
-  //     currentLength + 10 // 每次載入10個活動
-  //   );
-
-  //   if (newVisibleActivities.length === 0) {
-  //     setHasMore(false); // 沒有更多活動可載入
-  //   } else {
-  //     setVisibleActivities([...visibleActivities, ...newVisibleActivities]);
-  //   }
-  // };
-
-  // Function to generate star ratings
-const generateStars = (rating) => {
-  let stars = [];
-  for (let i = 0; i < rating; i++) {
-    stars.push(<img key={i} src="../star.png" alt="Star" className="star" />);
-  }
-  return stars;
-};
-
-
-if (isLoading) return <div>Loading...</div>;
+if (isLoading) {
+  return <LoadingSpinner />;
+}
   if (isError) return <div>Error: {error.message}</div>;
 
 
@@ -68,6 +44,12 @@ if (isLoading) return <div>Loading...</div>;
   //   console.error('activitiesData is not an array:', activitiesData);
   //   return <div>Error: Data is not an array</div>;
   // }
+
+  const navigate = useNavigate();
+
+  const handleAllActivityClick = (activityId) => {
+    navigate(`/activity/${activityId}`);
+};
 
   const activities = activitiesData?.activity;
 
@@ -83,14 +65,8 @@ if (isLoading) return <div>Loading...</div>;
     <div>
       <Header title="活動" showSortIcon={true}/>
       <div className="activity-list">
-        {/* <InfiniteScroll
-          dataLength={visibleActivities.length}
-          next={loadMoreActivities}
-          hasMore={hasMore}
-          loader={<h4>Loading...</h4>}
-        > */}
           {activities?.map((activity) => (
-            <div key={activity.id} className="activity-item">
+            <div key={activity.id} className="activity-item" onClick={() => handleAllActivityClick(activity.id)}>
               <img src={activity.picture} alt={activity.title} />
               <div className="activity-info">
                 <div className="title-time">
