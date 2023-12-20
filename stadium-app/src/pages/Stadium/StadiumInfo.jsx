6cfcfc8f09707df1289aeaf0f042edc3f6233355
yihsuan <yiiihsuann@gmail.com>
2023-12-20 14:44:08 +0800

    fix: reservation all

--- stadium-app/src/pages/Stadium/StadiumInfo.jsx
+++ stadium-app/src/pages/Stadium/StadiumInfo.jsx
@@ -14,17 +14,16 @@ const StadiumInfo = () => {
 
   const navigate = useNavigate();
 
-  
 
   const [category, setCategory] = useState('');
   const [stadiumId, setStadiumId] = useState('');
-  const [selectedDate, setSelectedDate] = useState(''); // 假设有 selectedDate
+  const [selectedDate, setSelectedDate] = useState(''); 
 
-  // 从 localStorage 中获取数据
+
   useEffect(() => {
     const retrievedStadiumId = localStorage.getItem('selectedStadiumId');
     const retrievedCategory = localStorage.getItem('selectedCategory');
-    const retrievedDate = localStorage.getItem('selectedDate'); // 假设有 selectedDate
+    const retrievedDate = localStorage.getItem('selectedDate'); 
 
     setStadiumId(retrievedStadiumId);
     setCategory(retrievedCategory);
@@ -32,10 +31,10 @@ const StadiumInfo = () => {
 
     console.log('Stadium ID:', retrievedStadiumId);
     console.log('Category:', retrievedCategory);
-    console.log('Selected Date:', retrievedDate); // 打印日期
+    console.log('Selected Date:', retrievedDate); 
   }, []);
 
-  // 使用 useQuery 获取数据
+
   const { data, isLoading, isError, error } = useQuery({
     queryKey: ['fetchStadiumAvailable', category, stadiumId, selectedDate],
     queryFn: () => fetchStadiumAvailable(category, stadiumId, selectedDate),
@@ -51,7 +50,6 @@ const StadiumInfo = () => {
 
 
   const handleConfirmReservation = () => {
-    // 在這裡處理確認預約的邏輯
     console.log('確認預約');
     navigate('/stadium/create');
   };
@@ -65,22 +63,25 @@ const StadiumInfo = () => {
       {data && data.data && (
         <div className="centered-textbox">
           <div className="centered-text bold-text large-text">{data.data.name}</div>
-          <div>
-            {data.data.water === 1 && <FaGlassWaterDroplet />}
-            {data.data.bathroom === 1 && <MdOutlineFamilyRestroom />}
-            {data.data.air_condition === 1 && <TbAirConditioning />}
-            {data.data.vending === 1 && <LuParkingCircle />}
+          <div className="icon-container">
+            {data.data.water === 1 && <FaGlassWaterDroplet  className="icon" />}
+            {data.data.bathroom === 1 && <MdOutlineFamilyRestroom className="icon"  />}
+            {data.data.air_condition === 1 && <TbAirConditioning className="icon"  />}
+            {data.data.vending === 1 && <LuParkingCircle className="icon"  />}
           </div>
           <div className="introduction-text">場地規則：{data.data.rule}</div>
+          {data.data.picture && (
+            <img src={data.data.picture} alt="場地圖片" className="info-stadium-image" />
+          )}
         </div>
       )}
       <div style={{ textAlign: 'center', marginTop: '20px' }}>
-        <button
-          onClick={handleConfirmReservation}
-          style={{ padding: '10px 20px', fontSize: '16px' }}
-        >
-          確認預約
-        </button>
+      <button
+  onClick={handleConfirmReservation}
+  className="confirm-reservation-button"
+>
+  確認預約
+</button>
       </div>
       <FooterBar />
     </div>
@@ -89,57 +90,3 @@ const StadiumInfo = () => {
 
 export default StadiumInfo;
 
-
-// 這是好的版本
-// import React from 'react';
-// import './StadiumInfo.scss';
-// import FooterBar from '../../components/FooterBar/FooterBar.jsx';
-// import Header from '../../components/Header/Header.jsx';
-// import { FaGlassWaterDroplet } from "react-icons/fa6";
-// import { MdOutlineFamilyRestroom } from "react-icons/md";
-// import { TbAirConditioning } from "react-icons/tb";
-// import { LuParkingCircle } from "react-icons/lu";
-// import { useNavigate } from 'react-router-dom';
-
-// const StadiumInfo = () => {
-
-//   const navigate = useNavigate();
-
-//   const handleConfirmReservation = () => {
-//     // 在這裡處理確認預約的邏輯
-//     console.log('確認預約');
-//     navigate('/stadium/create');
-//   };
-
-
-
-//   return (
-//     <div>
-//       <Header title="場地資訊" showSortIcon={true} />
-//       <div className="centered-textbox">
-//         <div className="centered-text bold-text large-text">羽球場A</div>
-//         <div>
-//             <FaGlassWaterDroplet />
-//             <MdOutlineFamilyRestroom />
-//             <TbAirConditioning />
-//             <LuParkingCircle />
-//         </div>
-//         <div className="introduction-text">場地規則：請詳閱注意事項</div>
-//       </div>
-
-//       {/* 添加確認預約按鈕 */}
-//       <div style={{ textAlign: 'center', marginTop: '20px' }}>
-//         <button
-//           onClick={handleConfirmReservation}
-//           style={{ padding: '10px 20px', fontSize: '16px' }}
-//         >
-//           確認預約
-//         </button>
-//       </div>
-
-//       <FooterBar />
-//     </div>
-//   );
-// };
-
-// export default StadiumInfo;
